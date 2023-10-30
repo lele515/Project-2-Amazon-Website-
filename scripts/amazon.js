2c7cdf103660b93c88648894e89a40b9167a0b40
lele515 <lemasafi15@gmail.com>
2023-10-30 09:46:37 -0600

    use-product-js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,36 +1,3 @@
-const products = [{
-image: 'images/ratings/rating-45.png',
-name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-rating: {
-    stars: 4.5,
-    count: 87
-},
-priceCents: 1090
-},{
-image: 'images/products/intermediate-composite-basketball.jpg',
-name:  'Intermediate Size Basketball',
-rating: {
-    stars: 4.0,
-    count: 127
-},
-priceCents: 2095
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:  'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 57
-    },
-    priceCents: 799
-},{
-  image: 'images/products/black-2-slot-toaster.jpg',
-  name: '2 Slot Toaster - Black',
-  rating: {
-    stars: 5,
-    count: 2197
-  },
-  priceCents: 1899,
-}];
 
 let productsHTML = '';
 
